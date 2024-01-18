a73010813071d6830ea9b822eec19e13392f0b19
Ugnius <uspokavicius@gmail.com>
2024-01-18 12:07:21 +0200

    asdasdasdas

--- game.js
+++ game.js
@@ -1,13 +1,17 @@
 // poker logic
-function createDeck() {
-    const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
-    const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
-    const deck = [];
+var deck;
+var hand;
+
+function createDeck() {
+    const suits = ["hearts", "diamonds", "clubs", "spades"];
+    const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
+
+    deck = [];
     let card = 0;
     for(let i = 0; i < suits.length; i++){
         for(let j = 0; j < ranks.length; j++){
-            let card = {Rank: ranks[j], Suit: suits[i]}
+            let card = `${ranks[j]}_of_${suits[i]}`;
             deck.push(card);
         }
     }
@@ -28,17 +32,37 @@ function shuffle(deck){
 }
 
 function dealCards(deck){
-    var hand = new Array(2);
+    hand = new Array(2);
     for(let i = 0; i < 2; i++){
         hand[i] = deck.pop();
     }
     return hand;
 }
 
-let deck = [];
-deck = createDeck();
-shuffle(deck);
-let hand = [];
-hand = dealCards(deck);
-console.log(hand);
+function renderHand(hand){
+    const handDisplay = document.getElementById('handDisplay');
+    handDisplay.innerHTML = ''; // Clear previous content
+
+    for (let i = 0; i < hand.length; i++) {
+        const cardImage = document.createElement('img');
+        cardImage.src = `playCards/${hand[i]}.svg`; // Assuming images are in the "cards" directory
+        cardImage.alt = hand[i];
+        cardImage.width = 100; // Set the desired width
+
+        handDisplay.appendChild(cardImage);
+    }
+}
+
+function startGame(){
+    deck = createDeck();
+    shuffle(deck);
+    hand = [];
+    hand = dealCards(deck);
+    
+    console.log(hand);
+
+    renderHand(hand);
+}
+
+
 
